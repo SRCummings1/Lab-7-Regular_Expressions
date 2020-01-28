46c6f1f19259f6afa7baa56ff8ed814dc8af303d
Sean <srcummings85@gmail.com>
2020-01-28 06:41:13 -0500

    My first Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7_Regular_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // method that will validate names, Names can only have alphabets, they should start with a capital letter, and they have a maximum length of 30.
+        }
+    }
+}
